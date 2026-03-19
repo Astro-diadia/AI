ee3755a7598b2e47e04a151babd50006355be343
Astro-diadia <nicu.vbcrupnii@gmail.com>
2026-03-19 16:04:24 +0200

    Update LLMCore.py

--- LLMCore/LLMCore.py
+++ LLMCore/LLMCore.py
@@ -2,11 +2,12 @@ from llama_cpp import Llama
 from json import loads
 import threading
 import queue
+from time import sleep
 from os import environ
 
 environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-# py "D:\LLMCore\LLMCore.py"
+# py "D:\AI\LLMCore\LLMCore.py"
 
 class LLMCore:
     def __init__(self):
@@ -23,10 +24,13 @@ class LLMCore:
             verbose=False,
             stream=True
         )
+
+        self.buffer = []
         
         self.queue = queue.Queue()
         self.output_queue = queue.Queue()
 
+        self.done = False
         self.worker = threading.Thread(
             target=self._llm_worker,
             daemon=True
@@ -34,19 +38,34 @@ class LLMCore:
         self.worker.start()
 
     def _llm_worker(self):
-        return self.model.create_chat_completion(
-                messages = [
-                    {
-                        "role": "system",
-                        "content": "you are an ai assistant",
-                    },
-                    {
-                        "role": "user",
-                        "content": prompt,
-                    }
-                ],
-                max_tokens=100
-            )['choices'][0]['message']['content']
+        while not self.done:
+            prompt = self.queue.get()
+
+            if prompt is None:
+                break
+
+            for llm_output in self.model.create_chat_completion(
+                    messages = [
+                        {
+                            "role": "system",
+                            "content": "you are an ai assistant",
+                        },
+                        {
+                            "role": "user",
+                            "content": prompt,
+                        }
+                    ],
+                    max_tokens=100
+                )['choices'][0]['message']['content']:
+                self.buffer.append(llm_output)
+
+            self.queue.task_done()
+
+            sleep(0.1)
+        self.done = False
+
+    def llm_buffer(self):
+
 
     def generate(self, prompt):
         self.queue.put(prompt)
@@ -54,3 +73,16 @@ class LLMCore:
         llm_output = self.output_queue.get()
 
         return llm_output
+
+    def get(self):
+        return self.output_queue.get()
+
+    def stop(self):
+        self.queue.clear()
+        self.output_queue.clear()
+        self.done = True
+        self.worker.join()
+
+LLMCore = LLMCore()
+while True:
+    print("llm said: ", LLMCore.generate("hello, what is the role of an orchestrator in an ai app"), "\n")
